2af82e349e3a615eafea2edf1824eb214302f016
Dhimas Bagus Prayoga <dhimasbagusprayoga@gmail.com>
2020-08-10 22:26:47 +0700

    module: jenkins: rebase

--- modules/jenkins/main.sh
+++ modules/jenkins/main.sh
@@ -24,8 +24,18 @@ source base/telegram_send.sh
 
 module_trigger() {
 
+while [ "${#}" -gt 0 ]; do
+  	case "${1}" in
+          *)
+                MESSAGE="${1}"
+  		shift
+  		;;
+  	esac
+  	shift
+  done
+
 trigger_help() {
-	tg_send_message --chat_id "$(tg_get_chat_id "$@")" --text "$2
+	tg_send_message --chat_id "$(tg_get_chat_id "$MESSAGE")" --text "$2
 
 Usage: \`!trigger [arguments] [value]\`
  \`-s\` (sync yes/no) (optional)
@@ -35,7 +45,7 @@ Usage: \`!trigger [arguments] [value]\`
  \`-b\` (build type user/userdebug/eng) (optional)
  \`-t\` (traget komodo/SystemUI/Settings) (optional)
  \`-job\` (paralel build 1-8) (optional)
- \`-sf\` (sourforge upload test/release) (optional)" --reply_to_message_id "$(tg_get_message_id "$@")" --parse_mode "Markdown"
+ \`-sf\` (sourforge upload test/release) (optional)" --reply_to_message_id "$(tg_get_message_id "$MESSAGE")" --parse_mode "Markdown"
  exit
 }
 
@@ -44,7 +54,7 @@ trigger_parse_arguments() {
   while [ "${#}" -gt 0 ]; do
   	case "${1}" in
           -h | --help )
-  		trigger_help "$@"
+  		trigger_help "$MESSAGE"
   		;;
           -p | --project )
   		PROJECT="${2}"
@@ -82,7 +92,7 @@ trigger_parse_arguments() {
   done
 }
 
-trigger_parse_arguments $(tg_get_command_arguments "$@")
+trigger_parse_arguments $(tg_get_command_arguments "$MESSAGE")
 
 if [ "$PROJECT" = "" ]; then
   PROJECT=komodo-release
@@ -121,10 +131,10 @@ if [ "$SERVER" = "" ]; then
 fi
 
 if [ "$TOKEN_JENKINS" = "" ]; then
-  tg_send_message --chat_id "$(tg_get_chat_id "$@")" --text "fatal: error: token Jenkins not available in variable" --reply_to_message_id "$(tg_get_message_id "$@")" --parse_mode "Markdown"
+  tg_send_message --chat_id "$(tg_get_chat_id "$MESSAGE")" --text "fatal: error: token Jenkins not available in variable" --reply_to_message_id "$(tg_get_message_id "$@")" --parse_mode "Markdown"
   exit
 elif [ "$DEVICE" = "" ]; then
-  trigger_help "$@" "Missing, arguments -d (device) is required"
+  trigger_help "$MESSAGE" "Missing, arguments -d (device) is required"
 else
 # Post api
   curl https://jenkins.komodo-os.my.id/job/$PROJECT/buildWithParameters \
@@ -139,8 +149,8 @@ else
   --form upload_to_sf=$SF \
   --form server=$SERVER
 
-  tg_send_message --chat_id "$(tg_get_chat_id "$@")" --text "Successful trigger jenkins for $DEVICE.
-We wish you a successful build and land safely" --reply_to_message_id "$(tg_get_message_id "$@")" --parse_mode "Markdown"
+  tg_send_message --chat_id "$(tg_get_chat_id "$MESSAGE")" --text "Successful trigger jenkins for $DEVICE.
+We wish you a successful build and land safely" --reply_to_message_id "$(tg_get_message_id "$MESSAGE")" --parse_mode "Markdown"
 fi
 
 }
